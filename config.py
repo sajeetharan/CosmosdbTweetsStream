e8a5e406756fc342b4815ff0649cb5827e393fa0
sajeetharan <sajeetharan@outlook.com>
2018-05-27 13:41:44 +0530

    Adding Files - Commit

---
+++ config.py
@@ -0,0 +1,15 @@
+
+# Enter CosmosDB config details below.
+masterKey = '' 
+host = ''
+
+#	Enter your database, collection and preferredLocations here.
+databaseId = 'tweepyDemo'
+collectionId = 'tweets'
+preferredLocations = ''
+
+# Enter twitter OAuth keys here.
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
